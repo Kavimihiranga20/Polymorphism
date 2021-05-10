dfc179bcafe09f46b3fa3119e02843eae8bfe413
kavimihiranga20 <kmvithanage48@gmail.com>
2021-05-10 13:03:50 +0530

    Frist commit

---
+++ Consoleprg4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Consoleprg4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            child child1 = new child();
+            child1.voice();
+        }
+
+        class parent
+        {
+
+            public virtual void voice()
+            {
+
+                Console.WriteLine("Loud Voice");
+            
+            }
+        
+        }
+
+        class child : parent
+        {
+
+            public override void voice()
+            {
+                Console.WriteLine("soft Voice");
+            
+            }
+        
+        }
+
+
+
+
+
+
+    }
+}
